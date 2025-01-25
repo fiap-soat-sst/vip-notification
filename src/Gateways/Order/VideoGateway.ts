4de6bb14355d8950906e210cd1d909e6401b9748
devtomasjr <dev@tomasjr.com>
2025-01-25 19:24:28 -0300

    feat: Implement User and Video entities, update Email handling, and enhance Notification routes

--- src/Gateways/Order/VideoGateway.ts
+++ src/Gateways/Order/VideoGateway.ts
@@ -1,11 +1,16 @@
 import { Either } from '../../@Shared/Either'
+import Video from '../../Entities/Video'
 import { IVideoRepository } from '../../External/Database/Repositories/Contracts/IVideoRepository'
 import { IVideoGateway } from '../contracts/IVideoGateway'
 
 export default class VideoGateway implements IVideoGateway {
     constructor(private readonly gateway: IVideoRepository) {}
 
-    findVideo(email: string, videoId: string): Promise<Either<Error, string>> {
+    findVideo(email: string, videoId: string): Promise<Either<Error, Video>> {
         return this.gateway.findVideo(email, videoId)
+    }
+
+    createVideoExample(): Promise<Either<Error, string>> {
+        return this.gateway.createVideoExample()
     }
 }
